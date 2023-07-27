543222719e37d0e7cfcf9e37bf783ba831ff50ef
Zheng Wang <zhengwang@Zhengs-MacBook-Pro.local>
2023-07-27 16:01:34 -0400

    questioned

--- answer.js
+++ answer.js
@@ -24,12 +24,78 @@
 // If a number is divisible by 5 log "Buzz" instead of the number.
 // If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
 
-for(let i = 1; i <=100; i++){
-    if(i % 3 === 0 && i % 5 === 0) {
-        console.log(i + " FizzBuzz")
-    }else if(i % 3 === 0){
-        console.log(i + " Fizz")
-    }else  if(i % 5 === 0){
-        console.log(i + " Buzz")
-    }   
-}
+// for(let i = 1; i <=100; i++){
+//     if(i % 3 === 0 && i % 5 === 0) {
+//         console.log(i + " FizzBuzz")
+//     }else if(i % 3 === 0){
+//         console.log(i + " Fizz")
+//     }else  if(i % 5 === 0){
+//         console.log(i + " Buzz")
+//     }   
+// }
+
+// Wild Wild Life
+//const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+// const sharky = ["Sharky", "shark", 20, "Left Coast"]
+// const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+// const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+//Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+// plantee[2] = 5001
+// console.log(plantee)
+
+// Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+// wolfy[3] = "Gotham City"
+// console.log(wolfy)
+// wolfy.pop()
+// console.log(wolfy)
+// wolfy.push("Gotham City")
+// console.log(wolfy)
+//?????????why cannot work use .replace()
+
+// Give D'Art a second hometown by adding "Hawkins"
+// dart.push("Hawkins")
+// console.log(dart)
+
+// wolfy.shift()
+// console.log(wolfy)
+// wolfy.unshift("Gameboy")
+// console.log(wolfy)
+
+// Yell at the Ninja Turtles  
+// const turtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"]
+// for(let k of turtles){
+//    console.log(k.toUpperCase()
+//   )
+// }
+
+// console.log(turtles.toUpperCase())
+
+// Methods, Revisited
+const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+// for(let k in favMovies){
+//     if(favMovies[k] === 'Titanic'){
+//         console.log(k)
+//     }
+// }
+favMovies.sort()  //will change orgianl array
+console.log(favMovies)
+favMovies.pop()
+console.log(favMovies)
+favMovies.push("Guardians of the Galaxy")
+console.log(favMovies)
+favMovies.reverse()
+console.log(favMovies)
+favMovies.shift()
+console.log(favMovies)
+favMovies.unshift("Baby Shark")
+console.log(favMovies)
+
+//splice"Django Unchained" and add "Avatar" (try finding the index of "Django Unchained", instead of counting it yourself) Thought question: did this permanently alter our array?
+// for (let k in favMovies){
+//     if(favMovies[k] == "Django Unchained"){
+//         console.log(k)
+//     }
+// }
+console.log(favMovies.splice(16,0,"Avatar"))
+console.log(favMovies)

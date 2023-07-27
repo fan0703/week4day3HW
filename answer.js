a26fc080b96c48122fb95442a0a82049605b5593
Zheng Wang <zhengwang@Zhengs-MacBook-Pro.local>
2023-07-27 13:43:49 -0400

    Fizz Buzz answered

--- answer.js
+++ answer.js
@@ -1,20 +1,20 @@
 // Easy Going
 // Write a for loop that will log the numbers 1 through 20.
 
-for(let i = 1; i <= 20; i++){
-    console.log(i)
-}
+// for(let i = 1; i <= 20; i++){
+//     console.log(i)
+// }
 
 // Get Even
 // Write a for loop that will log only the even numbers in 0 through 200.
 
 // Hint: Think about the increment expression.
 
-for(let i = 1; i <=200; i++){
-    if (i % 2 == 0){
-        console.log(i)
-    }
-}
+// for(let i = 1; i <=200; i++){
+//     if (i % 2 == 0){
+//         console.log(i)
+//     }
+// }
 
 // Fizz Buzz
 // This is a classic problem that you should get really comfortable solving. If you've solved it before, try to make it more elegant and short.
@@ -24,13 +24,12 @@ for(let i = 1; i <=200; i++){
 // If a number is divisible by 5 log "Buzz" instead of the number.
 // If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
 
-// for(let i = 1; i <=100; i++){
-//     if(i % 3 == 0 && i % 5 == 0) {
-//         console.log(i + " FizzBuzz")
-//     }
-//     if(i % 3 == 0){
-//         console.log(i + " Fizz")
-//     }if(i % 5 == 0){
-//         console.log(i + " Buzz")
-//     }   
-// }
+for(let i = 1; i <=100; i++){
+    if(i % 3 === 0 && i % 5 === 0) {
+        console.log(i + " FizzBuzz")
+    }else if(i % 3 === 0){
+        console.log(i + " Fizz")
+    }else  if(i % 5 === 0){
+        console.log(i + " Buzz")
+    }   
+}

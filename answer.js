3eed3e604e569512b81a3be5f9c0e4e709aaae13
Fan Yang <yangfanhewangzheng@gmail.com>
2023-07-27 16:09:57 -0400

    Fizz Buzz answered

--- answer.js
+++ answer.js
@@ -16,23 +16,23 @@ for(let i = 1; i <=200; i++){
     }
 }
 
-// Fizz Buzz
-// This is a classic problem that you should get really comfortable solving. If you've solved it before, try to make it more elegant and short.
+Fizz Buzz
+This is a classic problem that you should get really comfortable solving. If you've solved it before, try to make it more elegant and short.
 
-// Write a javascript application that logs all numbers from 1 - 100.
-// If a number is divisible by 3 log "Fizz" instead of the number.
-// If a number is divisible by 5 log "Buzz" instead of the number.
-// If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
+Write a javascript application that logs all numbers from 1 - 100.
+If a number is divisible by 3 log "Fizz" instead of the number.
+If a number is divisible by 5 log "Buzz" instead of the number.
+If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
 
-// for(let i = 1; i <=100; i++){
-//     if(i % 3 === 0 && i % 5 === 0) {
-//         console.log(i + " FizzBuzz")
-//     }else if(i % 3 === 0){
-//         console.log(i + " Fizz")
-//     }else  if(i % 5 === 0){
-//         console.log(i + " Buzz")
-//     }   
-// }
+for(let i = 1; i <=100; i++){
+    if(i % 3 === 0 && i % 5 === 0) {
+        console.log(i + " FizzBuzz")
+    }else if(i % 3 === 0){
+        console.log(i + " Fizz")
+    }else  if(i % 5 === 0){
+        console.log(i + " Buzz")
+    }   
+}
 
 // Wild Wild Life
 //const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]

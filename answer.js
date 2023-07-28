dd3b89b2476c84eeba43745ca4841c3f9062e06e
Fan Yang <yangfanhewangzheng@gmail.com>
2023-07-27 22:19:30 -0400

    Where is Waldo answered

--- answer.js
+++ answer.js
@@ -72,40 +72,40 @@
 // console.log(turtles.toUpperCase())
 
 // Methods, Revisited
-const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
-for(let k in favMovies){
-    if(favMovies[k] === 'Titanic'){
-        console.log(k)
-    }
-}
-favMovies.sort()  //will change orgianl array
-console.log(favMovies)
-favMovies.pop()
-console.log(favMovies)
-favMovies.push("Guardians of the Galaxy")
-console.log(favMovies)
-favMovies.reverse()
-console.log(favMovies)
-favMovies.shift()
-console.log(favMovies)
-favMovies.unshift("Baby Shark")
-console.log(favMovies)
+// const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+// for(let k in favMovies){
+//     if(favMovies[k] === 'Titanic'){
+//         console.log(k)
+//     }
+// }
+// favMovies.sort()  //will change orgianl array
+// console.log(favMovies)
+// favMovies.pop()
+// console.log(favMovies)
+// favMovies.push("Guardians of the Galaxy")
+// console.log(favMovies)
+// favMovies.reverse()
+// console.log(favMovies)
+// favMovies.shift()
+// console.log(favMovies)
+// favMovies.unshift("Baby Shark")
+// console.log(favMovies)
 
 //splice"Django Unchained" and add "Avatar" (try finding the index of "Django Unchained", instead of counting it yourself) Thought question: did this permanently alter our array?
-for (let k in favMovies){
-    if(favMovies[k] == "Django Unchained"){
-        console.log(k)
-    }
-}
-console.log(favMovies.splice(16,0,"Avatar"))
-console.log(favMovies)
+// for (let k in favMovies){
+//     if(favMovies[k] == "Django Unchained"){
+//         console.log(k)
+//     }
+// }
+// console.log(favMovies.splice(16,0,"Avatar"))
+// console.log(favMovies)
 //it permanently alter the array
 
 // slicethe last half of the array (challenge yourself and try to programatically determine the middle of the array rather than counting it and hard coding it) - Thought question: did this permanently alter our array?
 // console.log()
-console.log(favMovies.length /2)
-console.log(favMovies.slice(10))
-console.log(favMovies)  //not change original array
+// console.log(favMovies.length /2)
+// console.log(favMovies.slice(10))
+// console.log(favMovies)  //not change original array
 
 //console.log the index of "Fast and Furious" -We removed it from the array, what value do we get when we look for the index of something that is not in the array?
 
@@ -114,10 +114,20 @@ console.log(favMovies)  //not change original array
 //         console.log(j)
 //     }
 // }
-favMovies.splice(14, 1)
-console.log(favMovies)
-for(let m in favMovies){
-    if(favMovies[m] === "Fast and Furious"){
-        console.log(m)
-    }
-}
+// favMovies.splice(14, 1)
+// console.log(favMovies)
+// for(let m in favMovies){
+//     if(favMovies[m] === "Fast and Furious"){
+//         console.log(m)  //if we look for the index of something that is not in the array, it will output the array
+//     }
+// }
+
+// Where is Waldo
+const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
+                    ["Lucinda", "Jacc", "Neff", "Snoop"],
+                    ["Petunia", ["Baked Goods", "Waldo"]]];
+ whereIsWaldo.splice(1,1)  
+ console.log(whereIsWaldo)    
+ whereIsWaldo[1][2] = "No One" 
+ console.log(whereIsWaldo)  
+ console.log(whereIsWaldo[2][1][1])       
